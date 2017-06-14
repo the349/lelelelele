3366d0777ed10746491062d084acfc747619fccb
the349 <josephmalone25@gmail.com>
2017-06-14 09:32:56 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,29 @@
+//App.jsx
+const App = () => (
+  
+  {/* Views */}
+  
+    {/* Your main view, should have "main" prop */}	
+    
+      {/* Pages container, because we use fixed navbar and toolbar, it has additional appropriate props */}		  
+      		
+      {/* Initial Page /*}
+      
+        {/* Top Navbar /*}
+        
+        {/* Toolbar */}
+        
+        Link 1
+        Link 2
+        
+        {/* Page Content */}
+        
+Page content goes here
+
+
+        About App
+      
+      
+    
+  
+);
